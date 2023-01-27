5f714f40af28f9737debb2359600a8a10b6592f9
Abhishek@26 <ajaiswal@deqode.com>
2023-01-27 18:48:06 +0530

    cart schema to be fixed

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,31 @@
 const { Router } = require('express');
-const Product = require('../models/carts');
+const Cart = require('../models/carts');
 const router = require("express").Router();
 
 
 
-router.post("/checkout" , (req,resp)=>{
+async function register_order(order,username){
+    order_body = {}
+    order_body['userId'] = username
+    order_body['products'] = []
+    for (key in order){
+        temp = {}
+        temp['productId'] = key
+        temp['quantity'] = order[key]
+        order_body['products'].push(temp)
+    }
+
+    let data = new Cart(order_body);
+    let result = await data.save();
+
+    resp.send(result);
+
+}
+
+router.post("/checkout/:username" , (req,resp)=>{
+    resp.send(register_order(req.body, req.params.username))
     resp.send(req.body)
+
 })
 
 
